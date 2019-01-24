63b52489ee6bf56389337802e4e5a1de19e50c6a
Jonas Dorfinger <36422131+dorfingerjonas@users.noreply.github.com>
2019-01-24 17:59:04 +0100

    finished Correction

--- src/main/java/at/htl/bank/model/BankKonto.java
+++ src/main/java/at/htl/bank/model/BankKonto.java
@@ -2,5 +2,36 @@ package at.htl.bank.model;
 
 public class BankKonto {
 
-  
+  private String name;
+  private double kontoStand;
+
+  public BankKonto(String name, double kontoStand) {
+    this.name = name;
+    this.kontoStand = kontoStand;
+  }
+
+  public BankKonto(String name) {
+    this.name = name;
+    this.kontoStand = 0;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public double getKontoStand() {
+    return kontoStand;
+  }
+
+  public void einzahlen(double betrag) {
+    this.kontoStand += betrag;
+  }
+
+  public void abheben(double betrag) {
+    this.kontoStand -= betrag;
+  }
 }
